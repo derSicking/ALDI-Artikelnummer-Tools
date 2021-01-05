2b7610006c0027c08bd565f81b2b84575fc73cce
Frederik Sicking <frederik@dersicking.de>
2021-01-05 06:43:15 +0100

    Liste verbessert, autofocus, größerer Text

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -1,65 +1,76 @@
 oi = {};
 
-$(document).ready(function () {
+function nextLine() {
+	createClone();
+	$(".numberInput").last().focus();
+}
 
-	createClone = function(){
-	    clone = $(".form-container").first().clone(true);
-	    $(clone).find(".input").val("");
-	    $(clone).find(".numberInput").css("opacity", "100%");
-	    $(clone).find(".numberLabel").text("");
-	    oi = clone;
-	    clone.appendTo($("#allForms"));
+$(document).ready(function() {
+
+	createClone = function() {
+		clone = $(".form-container").first().clone(true);
+		$(clone).find(".input").val("");
+		$(clone).find(".numberInput").css("opacity", "100%");
+		$(clone).find(".numberLabel").text("");
+		oi = clone;
+		clone.appendTo($("#allForms"));
 	}
 
 	$(".numberInput").blur(function(event) {
-		if($(event.target).val().length <= 0){
+		if ($(event.target).val().length <= 0) {
 			return;
 		}
-	    $(event.target).animate({
-	    	opacity: "15%"
-	    }, 1000);
+		$(event.target).animate({
+			opacity : "15%"
+		}, 1000);
 	});
 	
-	$(".numberInput").focus(function(event) {
-	    $(event.target).animate({
-	    	width: "4em",
-	    	opacity: "100%"
-	    }, 1000);
+	$(".numberInput").click(function(event){
+		$(event.target).focus();
 	});
-	
-	$(".numberInput").on("input", function (event) {
 
-        event.preventDefault();
+	$(".numberInput").focus(function(event) {
+		$(event.target).animate({
+			width : "4em",
+			opacity : "100%"
+		}, 1000);
+	});
+
+	$(".numberInput").on("input", function(event) {
+
+		event.preventDefault();
 
 		input = $(event.target).val();
+		
+		if(input.length <= 0){
+			$(event.target.parentElement).find(".numberLabel").text("");
+			return;
+		}
 
-		ean = $.ajax({type: "GET", url: "/complete?an=" + input , async: false}).responseText;
+		ean = $.ajax({
+			type : "GET",
+			url : "/complete?an=" + input,
+			async : false
+		}).responseText;
 
-        $(event.target.parentElement).find(".numberLabel").text(ean);
+		$(event.target.parentElement).find(".numberLabel").text(ean);
 
-    });
-    
-    $(".input").keypress(function(event){
-    	var keycode = (event.keyCode ? event.keyCode : event.which);
-	    if(keycode == '13'){
-	    	createClone();
-	    	$(".numberInput").last().focus();
-	    }
-    });
-    
-    $("#plus").click(function(event){
-    	createClone();
-    });
-    
-    $(".minus").click(function(event) {
-    	if($(".form-container").length <= 1){
-    		// if there is only one line left in the list, clear that line but keep it
-    	    $(".form-container").first().find(".input").val("");
-    		return;
-    	}
+	});
+
+	$("#plus").click(function(event) {
+		nextLine();
+	});
+
+	$(".minus").click(function(event) {
+		if ($(".form-container").length <= 1) {
+			// if there is only one line left in the list, clear that line but
+			// keep it
+			$(".form-container").first().find(".input").val("");
+			$(".form-container").first().find(".numberLabel").text("");
+			$(".form-container").first().find(".input").focus();
+			return;
+		}
 		$(event.target).closest(".form-container").remove();
 	});
-    
-    $(".numberInput").first().focus();
-	
+
 });
